02a13c797e9d7c055faf67faf502cca1be6670dc
Cavallaro Catello Stefano <stefano@stepo.biz>
2023-01-03 11:34:44 +0100

    insert code

--- src/lib/common-lib.module.ts
+++ src/lib/common-lib.module.ts
@@ -1,16 +1,52 @@
 import { NgModule } from '@angular/core';
-import { CommonLibComponent } from './common-lib.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { StalAutocompleteModule } from '@stal/autocomplete';
+import { StalPaginatorModule } from '@stal/paginator';
+import { StalEventerModule } from '@stal/eventer';
 
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { MenuModule } from 'primeng/menu';
+import { RippleModule } from 'primeng/ripple';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { CalendarModule } from 'primeng/calendar';
+import { DialogService } from 'primeng/dynamicdialog';
+
+import { EngeCommonService } from './services/common.service';
 
 @NgModule({
-  declarations: [
-    CommonLibComponent
-  ],
-  imports: [
-  ],
-  exports: [
-    CommonLibComponent
-  ]
+	imports: [
+		/* add this 2 imports only in app module
+		BrowserModule,
+		HttpClientModule,
+		StalEventerModule,
+		*/
+	],
+	providers: [
+		EngeCommonService,
+		DialogService,
+	],
+	exports: [
+		CommonModule,
+
+		FormsModule,
+		ReactiveFormsModule,
+
+		//StalEventerModule, // <- Remove if is imported from main module TEST
+		StalAutocompleteModule,
+		StalPaginatorModule,
+
+		TableModule,
+		InputTextModule,
+		CalendarModule,
+		ButtonModule,
+		MenuModule,
+		RippleModule,
+		CardModule,
+
+	],
 })
-export class CommonLibModule { }
+export class EngeCommonLibModule { }
--- src/public-api.ts
+++ src/public-api.ts
@@ -2,6 +2,9 @@
  * Public API Surface of common-lib
  */
 
-export * from './lib/common-lib.service';
-export * from './lib/common-lib.component';
+export * from './lib/services/common.service';
+export * from './lib/components/generic.component';
+export * from './lib/components/generic-table.component';
+export * from './lib/components/generic-form.component';
+export * from './lib/form.validator';
 export * from './lib/common-lib.module';
